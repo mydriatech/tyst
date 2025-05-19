725581af578731f0aeb936ca5d2cb05f5ab0af07
mt-johan <172122831+mt-johan@users.noreply.github.com>
2025-05-19 15:37:55 +0200

    Add convinience function to digest trait to enable hashing of several items in one invocation.

--- tyst-traits/src/digest.rs
+++ tyst-traits/src/digest.rs
@@ -50,8 +50,14 @@ pub trait Digest: Send {
     /// Convinience method for ingesting the provided data and returning the
     /// output state (hash) in a single invocation.
     fn hash(&mut self, data: &[u8]) -> Vec<u8> {
-        self.update(data);
-        let mut out = vec![0u8; self.get_digest_size_bits() >> 3];
+        Self::hash_many(self, &[data])
+    }
+
+    /// Convinience method for ingesting the provided data and returning the
+    /// output state (hash) in a single invocation.
+    fn hash_many(&mut self, data: &[&[u8]]) -> Vec<u8> {
+        data.iter().for_each(|data| self.update(data));
+        let mut out = vec![0; self.get_digest_size_bits() / 8];
         self.finalize(&mut out);
         out
     }
